49163853729384a4aec0d87097ccf3d6b899b752
BenHamlen <hamle012@umn.edu>
2020-12-23 14:55:09 -0600

    Initial Commit

---
+++ openFAF/openFAFApp.swift
@@ -0,0 +1,17 @@
+//
+//  openFAFApp.swift
+//  openFAF
+//
+//  Created by Ben Hamlen on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct openFAFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
